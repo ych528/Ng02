c0e3cbba61743ec2eb205110b1ec04b8cbb19747
ych528idv <allenych528@gmail.com>
2019-01-29 18:39:07 +0800

    add 404 Page

--- tryRouting/src/app/app-routing.module.ts
+++ tryRouting/src/app/app-routing.module.ts
@@ -3,17 +3,33 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 // import { truncate } from 'fs';
 
 
-const routes: Routes = [{
-  path: 'home',
-  component: HomeComponent
-},
-{
-  path: 'about',
-  component: AboutComponent
-}];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'notfound',
+    component: NotfoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
