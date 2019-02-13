3de9537546b5f427fbdcd941cd492d9ecfdbe7e3
ych528idv <allenych528@gmail.com>
2019-02-13 13:49:37 +0800

    使用guard模擬Login效果

--- tryRouting/src/app/app-routing.module.ts
+++ tryRouting/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
@@ -61,7 +62,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     enableTracing: true,
-    useHash: false
+    useHash: false,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
--- tryRouting/src/app/layout/layout.guard.ts
+++ tryRouting/src/app/layout/layout.guard.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutGuard implements CanActivate {
+  constructor(private router: Router) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
     console.log("inputName", next.queryParams.username);
 
     const canActivate = next.queryParams.username === 'allen';
     if (!canActivate) {
       alert("you are not allow into");
+      this.router.navigate(['login']);
     }
     return canActivate;
   }
